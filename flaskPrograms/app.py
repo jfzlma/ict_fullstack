1112361c4d98039a19563e633e29f039ba93058a
ubuntu <jfzlma@gmail.com>
2018-11-13 16:17:43 +0530

    5th day, serverside usingflask

--- flaskPrograms/app.py
+++ flaskPrograms/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 
 app=Flask(__name__)
 
@@ -8,15 +9,27 @@ def hello():
 
 @app.route('/home')
 def home():
-    return 'Welcome Home'
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-    return 'About This Page '
-    
+    return render_template('about.html')
+
 @app.route('/contact')
 def contact():
-    return 'Contact us'
+    return render_template('contact.html')
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    if(request.method=='POST'):
+        getName = request.form['name']
+        getMobile = request.form['mobile']
+        getEmail = request.form['email']
+        getSub = request.form['subject']
+        getMes = request.form['message']
+        return render_template('result.html', newName=getName, newMobile=getMobile, newEmail=getEmail,newSub=getSub, newMes=getMes)
+
+
 
 if(__name__ == '__main__'):
     app.run(debug=True)
